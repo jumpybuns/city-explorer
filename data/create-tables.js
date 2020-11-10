637818455fdcc5e8721c4ea46a70a91899c461ef
Jumpybuns <ethanpiercepresents@gmail.com>
2020-11-09 16:09:45 -0800

    yayop

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,7 +17,7 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE location (
+                CREATE TABLE geos (
                     id SERIAL PRIMARY KEY NOT NULL,
                     formatted_query VARCHAR(256),
                     latitude VARCHAR(256),
